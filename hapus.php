b1297bf43b2bd4b6c97b9973d39f26e1e5013fc2
AwanMR <68161854+AwanMR@users.noreply.github.com>
2020-07-12 13:12:46 +0700

    menambah hapus.php

---
+++ hapus.php
@@ -0,0 +1,14 @@
+<?php
+if(isset($_GET['id'])) {
+   if(hapus($_GET['id'] > 0) {
+      echo "<script>
+          alert('data berhasil dihapus');
+          document.location.href='index.php';
+          </script>";
+   } else {
+      echo "<script>
+          alert('data gagal dihapus');
+          document.location.href='index.php';
+          </script>";
+   }
+}
